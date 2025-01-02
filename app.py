2be4e4f88b5e85a6c9f0e2f33d557ddc8325582f
najjahi <i.najjihi@hotmail.com>
2025-01-02 18:34:35 +0100

    modif 02/01

--- app.py
+++ app.py
@@ -2,10 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Active le mode de débogage explicitement
+app.config['DEBUG'] = True  # ou app.run(debug=True)
+
+@app.route('/')
+def home():
+    return "Bienvenue sur la page d'accueil !"
+    # La page CV est rendue par Flask avec le template 'cv.html'
+
 @app.route('/cv')
 def cv():
-    # La page CV est rendue par Flask avec le template 'cv.html'
-    return render_template('cv.html')
+    return render_template('cv.html')  # Rendre le fichier cv.html depuis le dossier templates  
 
 if __name__ == "__main__":
     app.run(debug=True)
